25826dfdc690dac134bfd7a97941d2e751432afc
solaomosola <olusolaomosola@yahoo.com>
2019-06-27 12:57:00 +0100

    No comment

--- magical.py
+++ magical.py
@@ -1,23 +1,23 @@
-#magic = [5,10,6,3,8,1]
-#dist =  [5,1,3,8,4,3]
+magic = [5,10,6,3,8,1]
+dist =  [5,1,3,8,4,3]
 
-magic=[8,4,1,9]
-dist = [10,9,3,5]
+#magic=[4,2,4,5,2]
+#dist = [4,4,3,1,3]
 
-def optimizeIt(start,magic,dist,path):
+def optimizeIt(magic,dist,path):
     
     ret_val=None
     path_len = len(path)
     for i in range(path_len):
         if ret_val == None: 
-            ret_val = magic[start] 
+            ret_val = magic[path[i]] 
         if i<path_len-1:
             ret_val = ret_val - dist[path[i]] + magic[path[i+1]]
         else:
             ret_val = ret_val - dist[path[i]]
-        #print(i,path_len,path,start,ret_val)
+        
     return ret_val
- 
+
 ret_index = -1    
 ret_val = None
 for start in range(len(magic)):
@@ -29,11 +29,16 @@ for start in range(len(magic)):
 
     #Check if its okay
     if (ret_val is None):
-        ret_val = optimizeIt(start,magic,dist,path)
+        ret_val = optimizeIt(magic,dist,path)
         if ret_val>=0:
             ret_index = start
-    current = optimizeIt(start,magic,dist,path)
-    if (ret_val> current and current>=0):
+    current = optimizeIt(magic,dist,path)
+    
+    
+    if (magic[start]==current):
+        ret_index = start
+        break
+    elif (ret_val> current and current>=0):
         ret_val = current
         ret_index = start
     
